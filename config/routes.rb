f56fe21de4a0adadb85d0d371a582811822a97f7
hallaman <heather.allaman@gmail.com>
2017-04-20 18:41:57 -0500

    root path at top of file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  root :to => 'welcome#index'
+
+
   resources :post_likes do
     put "upvote", to: "post_likes#upvote"
   end
@@ -33,9 +36,6 @@ Rails.application.routes.draw do
   resources :issues
   resources "contacts", only: [:new, :create]
 
-
-  
-  root :to => 'welcome#index'
 
   get 'welcome/index'
 
