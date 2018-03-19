483898edcd6dd20246c264d57342415ffeb3f916
hallaman <heather.allaman@gmail.com>
2018-03-19 16:30:33 -0500

    remove from controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,5 @@ class WelcomeController < ApplicationController
     client = Instagram.client(:access_token => '1517317818.1677ed0.daa3778c68b149cc82e9608c931a59d6')
     @insta = client.present? ? client.user_recent_media : ''
 
-    @testimonials = Testimonial.where(show: true)
-
   end
 end
