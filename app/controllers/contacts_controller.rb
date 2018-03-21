38fbc79cae7c1778b300395fb805d167ebcc2cd4
hallaman <heather.allaman@gmail.com>
2018-03-21 10:47:18 -0500

    cleanup

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,8 +12,9 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
+    # @contact.request = request
+    if @contact.valid?
+      @contact.deliver
       flash.now[:notice] = 'Thank you for your message, I will be in contact soon!'
     else
       flash.now[:error] = @contact.errors.full_messages.join(". ")
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,33 +89,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: 'gmail.com',
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: ENV['GMAIL_UN'],
-  #   password: ENV['GMAIL_PW']
-  # }
 
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
+    :domain => 'gmail.com',
     :user_name =>  ENV['GMAIL_UN'], # Your SMTP user here.
     :password => ENV['GMAIL_PW'], # Your SMTP password here.
     :authentication => :login,
     :enable_starttls_auto => true
   }
 
-  # config.paperclip_defaults = {
-  #   :storage => :s3,
-  #   :s3_host_name => 's3-us-west-2.amazonaws.com',
-  #   :s3_credentials => {
-  #     :bucket => ENV['AWS_BUCKET'],
-  #     :access_key_id => ENV['AWS_KEY_ID'],
-  #     :secret_access_key => ENV['AWS_ACCESS_KEY'],
-  #     :s3_region => 'us-west-2'
-  #   }
-  # }
 end
